b4aee22680c7dcffa9a5919031ae8a5c1ceb0065
OndraHa <Ondrejhadraba@gmail.com>
2023-04-15 17:00:14 +0200

    Initial Commit

---
+++ NasaAPOD/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NasaAPOD
+//
+//  Created by Ondřej Hadraba on 15.04.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NasaAPOD/NasaAPODApp.swift
@@ -0,0 +1,17 @@
+//
+//  NasaAPODApp.swift
+//  NasaAPOD
+//
+//  Created by Ondřej Hadraba on 15.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct NasaAPODApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
